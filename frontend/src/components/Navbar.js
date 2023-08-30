d3a914b8c9c2f96b55c322c1db6da92cef6b5529
Jason Nguyen <jasonnguyenphhs@gmail.com>
2023-08-29 21:20:53 -0700

    Co-authored-by: feli <felfeng@users.noreply.github.com>

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Navbar.css'
+import Logo from '../images/feliloops_logo.png'
 
 function Navbar() {
     return (
@@ -7,6 +8,7 @@ function Navbar() {
             <ul>
                 <li><a href="/" className='no-underline'><span className='title-box'>home</span></a></li>
                 <li><a href="/products" className='no-underline'><span className='title-box'>products</span></a></li>
+                <img className = 'logo' src={Logo} alt = 'description'/>
                 <li><a href="/about" className='no-underline'><span className='title-box'>about me</span></a></li>
                 <li><a href="/contact" className='no-underline'><span className='title-box'>contact</span></a></li>
             </ul>
--- frontend/src/components/Products.js
+++ frontend/src/components/Products.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import './Products.css';
-import { Link } from 'react-router-dom';
-import starCatBeanieImage from '../product_images/starbeanie.JPG'
+import { Link as RouterLink } from 'react-router-dom';
+import { Card, CardContent, Grid, Typography, CardMedia } from '@mui/material';
+import { Box } from '@mui/material';
+import starCatBeanieImage from '../product_images/starbea.jpg'
 
 const ProductItems = [
     {
@@ -16,20 +18,57 @@ const ProductItems = [
         image: starCatBeanieImage,
         price: "$18"
     },
+    {
+        id: 3,
+        name: "babydoll top",
+        image: starCatBeanieImage,
+        price: "$75"
+    },
+    {
+        id: 4,
+        name: "babydoll top",
+        image: starCatBeanieImage,
+        price: "$75"
+    },
+    {
+        id: 5,
+        name: "granny coaster",
+        image: starCatBeanieImage,
+        price: "$12"
+    },
 ];
 
 
 const Products = () => {
     return (
-        <div className="productgrid">
-            {ProductItems.map(product => (
-                <Link key={product.id} to={`/product/${product.id}`} className="product-item">
-                    <img src={product.image} alt={product.name} />
-                    <h3>{product.name}</h3>
-                    <p>{product.price}</p>
-                </Link>
+        <Grid container spacing={7} className="productgrid" style={{paddingTop: '75px'}}>
+            {ProductItems.map((product) => (
+                <Grid item lg={3} key={product.id}> 
+                    <RouterLink to={`/product/${product.id}`} className="no-outline">
+                        <Card style = {{borderRadius: '22px'}}>
+                            <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" height="100%">
+                                <CardMedia
+                                    component="img"
+                                    height="280"
+                                    width="100"
+                                    image={product.image}
+                                    alt={product.name}
+                                    sx={{ borderRadius: '22px' }}
+                                />
+                                <CardContent sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', p: 0 }}>
+                                    <Typography variant="h6" component="div" sx={{ fontFamily: 'Georgia, serif', textDecoration: 'none' }}>
+                                        {product.name}
+                                    </Typography>
+                                    <Typography variant="body1" sx={{ fontFamily: 'Georgia, serif', fontWeight: 'bold' }}>
+                                        {product.price}
+                                    </Typography>
+                                </CardContent>
+                            </Box>
+                        </Card>
+                    </RouterLink>
+                </Grid>
             ))}
-        </div>
+        </Grid>
     );
 }
 
