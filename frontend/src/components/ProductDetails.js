cd17eb928e8c85431211808d4931688007330a5f
felfeng <felfeng@ucdavis.edu>
2023-09-03 21:01:45 -0700

    more edits to prod description and added imgs

--- frontend/src/components/ProductDetails.js
+++ frontend/src/components/ProductDetails.js
@@ -1,16 +1,18 @@
 import { useParams } from 'react-router-dom';
 import { ProductItems } from './data';
+import './ProductDetails.css';
 
 function ProductDetails() {
   const { id } = useParams();
-
   const product = ProductItems.find(p => p.id === parseInt(id));
 
   return (
-    <div>
-      <h1>{product.name}</h1>
-      <img src={product.image} alt={product.name} />
-      <p>Price: {product.price}</p>
+    <div className="prod-details">
+      <img className="prod-details-img" src={product.image} alt={product.name} />
+      <div className="prod-info">
+        <h1>{product.detailedName}</h1>
+        <p>Price: {product.price}</p>
+      </div>
     </div>
   );
 }
--- frontend/src/components/data.js
+++ frontend/src/components/data.js
@@ -1,16 +1,19 @@
 import starCatBeanieImage from '../product_images/starbea.jpg';
+import hangingtulip from '../product_images/hanging-tulip.png';
+import gloves from '../product_images/fingerless-gloves.png';
 
 export const ProductItems = [
     {
         id: 1,
         name: "star cat beanie",
+        detailedName: "star cat beanie (garden gnome version!)",
         image: starCatBeanieImage,
         price: "$35"
     },
     {
         id: 2,
         name: "fingerless gloves",
-        image: starCatBeanieImage,
+        image: gloves,
         price: "$18"
     },
     {
@@ -31,4 +34,22 @@ export const ProductItems = [
         image: starCatBeanieImage,
         price: "$12"
     }, 
+    {
+        id: 6,
+        name: "strappy cat beanie",
+        image: starCatBeanieImage,
+        price: "$38"
+    }, 
+    {
+        id: 7,
+        name: "mushroom pouch",
+        image: starCatBeanieImage,
+        price: "$10"
+    },
+    {
+        id: 8,
+        name: "car hanging tulip",
+        image: hangingtulip,
+        price: "$25"
+    },
 ];
